3d6d57925b794113b50165973955f0d4b89e9b59
fahmi ahmad <fahmiahmad@tutanota.com>
2023-12-30 00:03:59 +0700

    lesson 7 exercise 3

--- nrf-connect-sdk-fundamentals/ncs-fund/v2.x.x/lesson7/fund_less7_exer3/src/main.c
+++ nrf-connect-sdk-fundamentals/ncs-fund/v2.x.x/lesson7/fund_less7_exer3/src/main.c
@@ -9,11 +9,14 @@
 #include <string.h>
 
 // Define stack size used by each thread
-#define THREAD0_STACKSIZE       512
-#define THREAD1_STACKSIZE       512
-#define WORQ_THREAD_STACK_SIZE  512
+#define THREAD0_STACKSIZE 512
+#define THREAD1_STACKSIZE 512
+#define WORQ_THREAD_STACK_SIZE 512
 
 /* STEP 2 - Set the priorities of the threads */
+#define THREAD0_PRIORITY 2
+#define THREAD1_PRIORITY 3
+#define WORKQ_PRIORITY 4
 
 // Define stack area used by workqueue thread
 static K_THREAD_STACK_DEFINE(my_stack_area, WORQ_THREAD_STACK_SIZE);
@@ -21,36 +24,67 @@ static K_THREAD_STACK_DEFINE(my_stack_area, WORQ_THREAD_STACK_SIZE);
 // Define queue structure
 static struct k_work_q offload_work_q = {0};
 
-
 /* STEP 5 - Define function to emulate non-urgent work */
-
+static inline void emulate_work()
+{
+    for (volatile int count_out = 0; count_out < 150000; count_out++)
+        ;
+}
 
 /* STEP 7 - Create work_info structure and offload function */
-
+struct work_info
+{
+    struct k_work work;
+    char name[25];
+} my_work;
+void offload_functin(struct k_work *work_tem)
+{
+    emulate_work();
+}
 
 void thread0(void)
 {
     uint64_t time_stamp;
     int64_t delta_time;
-	/* STEP 8 - Start the workqueue, */
-	/* initialize the work item and connect it to its handler function */ 
-    
-    while (1) {
+    /* STEP 8 - Start the workqueue, */
+    /* initialize the work item and connect it to its handler function */
+    k_work_queue_start(&offload_work_q, my_stack_area, K_THREAD_STACK_SIZEOF(my_stack_area), WORKQ_PRIORITY, NULL);
+    strcpy(my_work.name, "thread0 emulate_work()");
+    k_work_init(&my_work.work, offload_functin);
+
+    while (1)
+    {
         time_stamp = k_uptime_get();
-		/* STEP 9 - Submit the work item to the workqueue instead of calling emulate_work() directly */
-		/* Remember to comment out emulate_work(); */
-        emulate_work();
-		delta_time = k_uptime_delta(&time_stamp);
+        /* STEP 9 - Submit the work item to the workqueue instead of calling emulate_work() directly */
+        /* Remember to comment out emulate_work(); */
+        // emulate_work();
+        k_work_submit_to_queue(&offload_work_q, &my_work.work);
+
+        delta_time = k_uptime_delta(&time_stamp);
         printk("thread0 yielding this round in %lld ms\n", delta_time);
         k_msleep(20);
-    }   
+    }
+}
+
+void thread1()
+{
+    uint64_t time_stamp;
+    int64_t delta_time;
+
+    while (1)
+    {
+        time_stamp = k_uptime_get();
+        emulate_work();
+        delta_time = k_uptime_delta(&time_stamp);
+
+        printk("thread1 yielding this round in %lld ms\n", delta_time);
+        k_msleep(20);
+    }
 }
 
 /* STEP 4 - Define entry function for thread1 */
 
-
 K_THREAD_DEFINE(thread0_id, THREAD0_STACKSIZE, thread0, NULL, NULL, NULL,
-		THREAD0_PRIORITY, 0, 0);
+                THREAD0_PRIORITY, 0, 0);
 K_THREAD_DEFINE(thread1_id, THREAD1_STACKSIZE, thread1, NULL, NULL, NULL,
-		THREAD1_PRIORITY, 0, 0);
-
+                THREAD1_PRIORITY, 0, 0);
